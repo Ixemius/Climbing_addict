3d7ac823618d6188615d8e0b1d5f88036fd08889
Ixemius <timo29260@gmail.com>
2023-06-29 17:20:41 +0200

    ajout de la fonctionalité dyslexie

--- public/js/dyslexia.js
+++ public/js/dyslexia.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     let dyslexie = false;
     let dysButton = document.getElementById('dys');
-    let body = document.querySelector('body');
+
     dysButton.addEventListener('click', function () {
         dyslexie = !dyslexie;
         dysButton.classList.toggle('active');
         if (dyslexie)
-            body.classList.add('dyslexia');
+            document.body.classList.add('dyslexia');
         else {
-            body.classList.remove('dyslexia');
+            document.body.classList.remove('dyslexia');
         }
     });
 });
